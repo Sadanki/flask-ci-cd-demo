eb0d2a5fb6df7947fd3abf8953d6e948aaf83b92
Sadanki <Sadanki190@gmail.com>
2025-05-24 20:30:26 +0530

    Update app.py with a test to trigger

--- flask_app/app.py
+++ flask_app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, CI/CD Pipeline Working! 🚀" 
+    return "Hello, CI/CD Pipeline Working! This is Test to Trigger.🚀" 
 
 if __name__ == '__main__':
     app.run()
