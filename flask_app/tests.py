85d671d844077a37e1396355fa117a595469f2ea
Sadanki <Sadanki190@gmail.com>
2025-05-24 17:55:09 +0530

    Initial Flask application

---
+++ flask_app/tests.py
@@ -0,0 +1,12 @@
+import unittest
+from flask_app.app import app
+
+class FlaskTestCase(unittest.TestCase):
+    def test_hello(self):
+        tester = app.test_client(self)
+        response = tester.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, b'Hello, World!')
+
+if __name__ == '__main__':
+    unittest.main()
